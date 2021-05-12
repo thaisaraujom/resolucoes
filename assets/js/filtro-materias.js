c2e27fa5be72c58ffbff9e423bf4e86d3db74d9e
Thaís Araújo <thaisam2000@gmail.com>
2021-05-12 02:55:07 -0300

    #59-filtro das matérias parte 1

---
+++ assets/js/filtro-materias.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(".materias .materia").click(function(){
+      $(this).addClass("materia-selecionada borda").siblings().removeClass("materia-selecionada borda")
+      $(".selecao")
+      $(".modulos-todos").fadeOut()
+      setTimeout(function(){
+        $(".modulos-todos").fadeIn()
+      }, 200)
+
+      let value = $(this).attr("data-filter")
+
+  
+
+        if(value == "modulos-todos") {
+          $(".modulos-todos .modulos").hide()
+        } else {
+          $(".modulos-todos .modulos").not("." + value).hide()
+          $(".modulos-todos .modulos").filter("." + value).show("150")
+
+        }
+
+    })
+  })
