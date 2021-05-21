053757ef88e51abd6befffc1e017f97a464ab6dd
Thaís Araújo <thaisam2000@gmail.com>
2021-05-21 00:34:01 -0300

    #63-ajustes finais em aluno-desempenho e correção na filtragem em aluno-modulo

--- assets/js/filtro-materias.js
+++ assets/js/filtro-materias.js
@@ -1,6 +1,6 @@
 /* DESKTOP */
 // $(document).ready(function () {
-  
+
 //   $(".materias .materia").click(function () {
 //     $(this).addClass("materia-selecionada borda").siblings().removeClass("materia-selecionada borda")
 
@@ -50,11 +50,11 @@
 //    /* FIM MOBILE */
 // })
 
-let materia = document.querySelectorAll('.materia')
+let materia = document.querySelectorAll('.carousel .materia')
 let modulos = document.querySelectorAll('.modulos')
 
 materia.forEach((materia_single) => {
-  materia_single.addEventListener('click', function () {
+    materia_single.addEventListener('click', function () {
         materia.forEach((materia_single1) => {
             materia_single1.classList.remove('active')
             materia_single1.classList.remove('borda')
@@ -68,10 +68,44 @@ materia.forEach((materia_single) => {
         modulos.forEach((modulos_single) => {
             modulos_single.classList.remove('active')
             modulos_single.classList.add('hide')
+            modulos_single.classList.add('esconder-modulo')
 
             if (modulos_single.getAttribute('data-item') == dataFilter) {
                 modulos_single.classList.remove('hide')
                 modulos_single.classList.add('active')
+                modulos_single.classList.remove('esconder-modulo')
+            }
+        })
+
+    })
+
+})
+
+let materias_drop = document.querySelectorAll('.materias-drop .materia')
+
+materias_drop.forEach((materiaD_single) => {
+    materiaD_single.addEventListener('click', function () {
+        materias_drop.forEach((materiaD_single1) => {
+            materiaD_single1.classList.toggle('active')
+            materiaD_single1.classList.remove('borda-drop')
+            materiaD_single1.classList.remove('materia-selecionada-drop')
+        })
+
+        this.classList.add('active')
+        this.classList.add('materia-selecionada-drop')
+        this.classList.add('borda-drop')
+
+        let dataFilter = this.getAttribute('data-filter')
+
+        modulos.forEach((modulos_single) => {
+            modulos_single.classList.remove('active')
+            modulos_single.classList.add('hide')
+            modulos_single.classList.add('esconder-modulo')
+
+            if (modulos_single.getAttribute('data-item') == dataFilter) {
+                modulos_single.classList.remove('hide')
+                modulos_single.classList.add('active')
+                modulos_single.classList.remove('esconder-modulo')
             }
         })
 
