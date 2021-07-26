eb4591362b95414a83fed041b0487278e0ca92a5
Thaís Araújo <thaisam2000@gmail.com>
2021-07-25 23:56:04 -0300

    #100-admin-dashboard ajuste menu

--- assets/js/home-down.js
+++
@@ -1,6 +0,0 @@
-const conteudo_down = document.querySelector('.home-down')
-const down = document.querySelector('.icon-down')
-
-down.addEventListener('click', function(){
-    conteudo_down.classList.toggle('home-desativada')
-})
