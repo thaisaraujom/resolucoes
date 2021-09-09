ba6f92c0c68408a991898904837c63ad0491e636
Thaís Araújo <thaisam2000@gmail.com>
2021-09-09 02:55:23 -0300

    #110-ajustes cursinho-dashboard, 3 modais, cursinho-perfil

--- assets/js/cursinho-modais.js
+++ assets/js/cursinho-modais.js
@@ -1,5 +1,4 @@
 let cadastrar_d = document.querySelector('.button-cadastrar.button-disciplina-c')
-console.log(cadastrar_d);
 let modal_finalizar = document.querySelector('.modal-finalizar-overlay.modal1')
 let simbolo_sair = document.querySelector('.x-sair')
 
@@ -11,4 +10,19 @@ cadastrar_d.addEventListener('click', function () {
 simbolo_sair.addEventListener('click', function () {
     event.preventDefault()
     modal_finalizar.style.visibility = 'hidden'
+})
+
+/* --------------------------------------- MODAL 2 -----------------------------------*/
+let cadastrar_a = document.querySelector('.button-cadastrar.baluno')
+let modal_finalizar2 = document.querySelector('.modal-finalizar-overlay.modal2')
+let simbolo_sair2 = document.querySelector('.x-sair.x-sair1')
+
+cadastrar_a.addEventListener('click', function () {
+    event.preventDefault()
+    modal_finalizar2.style.visibility = 'visible'
+})
+
+simbolo_sair2.addEventListener('click', function () {
+    event.preventDefault()
+    modal_finalizar2.style.visibility = 'hidden'
 })
