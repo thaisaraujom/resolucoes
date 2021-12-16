9f544924798e937bdec6512552bb462c93fd0be7
pedropaulocg <pedropaulocostaguima@hotmail.com>
2021-12-16 12:05:08 -0300

    #02-correções

---
+++ assets/js/admin-dashboard-curso-grafico.js
@@ -0,0 +1,100 @@
+let data = []
+
+const response = fetch('').then(response => response.json).then(data => console.log(data));
+console.log(response)
+var options = {
+  grid: {
+    borderColor: "transparent",
+    strokeDashArray: 0,
+  },
+    series: [{
+    name: 'notas',
+    data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8]
+  }],
+    chart: {
+    height: 350,
+    type: 'bar',
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 0,
+      dataLabels: {
+        position: 'top', // top, center, bottom
+      },
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    colors: ["#282C35"],
+    opacity: 1,
+    type: "solid",
+  },
+
+  xaxis: {
+    categories: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    position: 'bottom',
+    axisBorder: {
+      show: true,
+      color: "#282C35",
+      height: 5,
+      width: "100%",
+      offsetX: 0,
+      offsetY: 0,
+      
+    },
+    crosshairs: {
+      fill: {
+        type: 'gradient',
+        gradient: {
+          colorFrom: '#F58E08',
+          colorTo: '#F58E08',
+          stops: [0, 100],
+          opacityFrom: .7,
+          opacityTo: .7,
+        }
+      }
+    },
+    labels:{
+      style: {
+        fontSize:["20px"],
+      },
+    },
+    axisTicks: {
+      show: false
+    },
+    tooltip: {
+      enabled: false,
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false,
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val + "%";
+      }
+    },
+  
+
+  },
+  title: {
+    text: '',
+    floating: true,
+    offsetY: -20,
+    align: 'center',
+    style: {
+      color: '#444',
+      fontSize: "27px",
+    },
+  },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
